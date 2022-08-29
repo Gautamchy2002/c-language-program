8e2026bebfb99e373fd3d6af58ee2ccc100fc46e
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2022-08-29 20:36:35 +0530

    Add files via upload

---
+++ tutorial23.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+//printf("Hello world\n");
+int marks[4];
+marks [1] = 7;
+printf("Marks of 1 student is %d\n", marks[1]);
+marks[1] = 48;
+printf("Marks of 2 student is %d\n", marks[1]);
+return 0;
+}
