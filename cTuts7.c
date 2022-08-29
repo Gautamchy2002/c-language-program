32bdabb94db50ee6921b853042c4f00655040ec2
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2022-08-29 20:25:17 +0530

    Add files via upload

---
+++ cTuts7.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a ;
+    float b ;
+    a= 45;
+    b= 25.56;
+    printf("a + b=%f\n", a+b);
+    printf("a - b=%f\n", a-b);
+    printf("a * b=%f\n", a*b);
+    printf("a / b=%f\n", a/b);
+    return 0;
+}
