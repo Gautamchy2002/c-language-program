7ad0e741906ffe63052e36e40ffae1ea65c1171c
Gautamchy2002 <112376740+Gautamchy2002@users.noreply.github.com>
2022-08-29 20:36:51 +0530

    Add files via upload

---
+++ unionsets.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define MAX 50
+int main ()
+{
+    int a[5], b[5], c[10], n1, n2, i, j ;
+    int flag;
+    // printf("enter the number of element of set A");
+    // scanf("%d ",&n1);
+    printf("enter the element of setA :\n");
+    for(i=0; i<5; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+// printf("enter the number of element of set B");
+//     scanf("%d ",&n2);
+    printf("enter the element of setB :\n");
+    for(i=0; i<5; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    for(i = 0; i<5; i++)
+    {
+        if( c[i]==a[i]);
+    
+    }
+
+    for (i = 0; i<size(B), i++)
+    {
+        flag = 0;
+        for (j = 0; j<size(A), j++)
+        if (setB[i]==setA[j])
+        {flag = 1, break}
+    }
+    if (flag = 0)
+    {setC[z++]=setB[i]}
+}
+
+            for (int i = 0; i < 10; i++)
+            {
+                printf("%d ", c[i]);
+            }
+
+
+    return 0;
+}
+
